7fed3d81c4d120698c148888a516a146380b9dd4
foo2810 <anime_117@yahoo.co.jp>
2019-10-07 20:56:21 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='ml',
+      version='1.0',
+      #description='Machine Learning Utilities',
+      #author='hogedamari',
+      #author_email='hogehoge@example.com',
+      packages=find_packages(),
+)
